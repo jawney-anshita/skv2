0d1bdffd4b4b241c60665eea9591d0e33d83a666
jawney-anshita <120613359+jawney-anshita@users.noreply.github.com>
2023-12-28 21:33:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
+
+     eslint: {
+        ignoreDuringBuilds: true,
+      },
+      typescript: {
+        ignoreBuildErrors: true,
+      },
+      reactStrictMode: true,
+      i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+      },
 }
 
 module.exports = nextConfig
